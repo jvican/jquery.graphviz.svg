88cd9995a133f64dee40bc1eb7cda04f5b944ffb
jvican <jorgevc@fastmail.es>
2017-11-07 11:11:50 +0100

    Use dot files instead of gv

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash - 
 set -o nounset                              # Treat unset variables as an error
 
-cp "$1" demo.gv
-dot -Tsvg -o demo.svg demo.gv
+cp "$1" demo.dot
+dot -Tsvg -o demo.svg demo.dot
